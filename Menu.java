54a464dbad4061f3f0b556710621f529f8fc4877
Felipe1885 <felipe.leonardi2005@gmail.com>
2024-11-27 21:31:04 -0300

    bug arrumado (eu acho) e wallpaper do menu com botao

---
+++ Menu.java
@@ -0,0 +1,50 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel {
+    private final JButton PlayButton;
+    private Game game;
+
+    public Menu() {
+        setLayout(null);
+        PlayButton = new JButton("New Game");
+        //int windowHeight = (int) (getSize().getHeight()/2);
+        //int windowWidth = (int) (getSize().getWidth()/2);
+    
+        PlayButton.setBounds(300,300,100,50);
+    
+        PlayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game = new Game(UI.getHighScore(), 0, 10, 20);
+                UI.gameScreen(game);
+            }
+        });
+        add(PlayButton);
+    }
+
+@Override 
+public void paintComponent(Graphics g) {
+    
+    int windowHeight = (int) (getSize().getHeight());
+    int windowWidth = (int) (getSize().getWidth());
+    //int imageOriginY = windowHeight/2 - imageHeight/2;
+
+        try {
+            BufferedImage background = ImageIO.read(new File("src\\TetrisMenuBackground.png"));
+            Image scaledBackground = background.getScaledInstance(windowWidth, windowHeight, BufferedImage.SCALE_DEFAULT);
+            g.drawImage(scaledBackground, 0, 0, this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- UI.java
+++ UI.java
@@ -1,37 +1,20 @@
 import java.awt.CardLayout;
 import java.awt.Container;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class UI extends JPanel {
     
-    private Container container;
-    private CardLayout cardlayout;
-    private final JButton PlayButton;
-    private final JPanel menu;
+    private static Container container;
+    private static CardLayout cardlayout;
     private Game game;
-    private int highScore = 0;
+    private static int highScore = 0;
     
     public UI() {
         cardlayout = new CardLayout();
         container = new JPanel(cardlayout);
         setLayout(cardlayout);
         add(container);
-
-        menu = new JPanel();
-        PlayButton = new JButton("New Game");
-        PlayButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                game = new Game(highScore, 0, 10, 20);
-                container.add(game, "Game");
-                cardlayout.show(container, "Game");
-                game.requestFocus();
-            }
-        });
-        menu.add(PlayButton);
+        Menu menu = new Menu();
         container.add(menu, "Menu");
     }
 
@@ -39,6 +22,16 @@ public class UI extends JPanel {
         highScore = game.getHighScore();
         cardlayout.show(container, "Menu");
         container.remove(game);
+    }
+
+    public static void gameScreen(Game game) {
+        container.add(game, "Game");
+        cardlayout.show(container, "Game");
+        game.requestFocus();
+    }
+
+    public static int getHighScore() {
+        return highScore;
     }
 }
 
